30800c73e884077ba3abfb0db6eb5e9d41fafbc5
Jaakko Husso <jaakko.husso@futurice.com>
2017-05-22 17:35:27 +0300

    Search entity by its name and return name + spawntime

--- src/services/search.js
+++ src/services/search.js
@@ -4,7 +4,10 @@ const knexConfig = require('../config/knexfile');
 
 const knex = Knex(knexConfig[process.env.NODE_ENV || 'development']);
 
-const searchByName = name => knex.select().from('creature_template').where('name', name);
+const searchByName = name => knex.select('creature.spawntimesecs', 'creature_template.name')
+  .from('creature')
+  .leftOuterJoin('creature_template', 'creature.id', 'creature_template.entry')
+  .where('creature_template.name', 'like', `%${name}%`);
 
 module.exports = {
   searchByName,
