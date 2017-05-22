86347ac280b278f94b925dfb36964b426141617c
Jaakko Husso <jaakko.husso@futurice.com>
2017-05-22 17:42:06 +0300

    Make config aceept cleardb from heroku

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const config = {
-  databaseUrl: process.env.DATABASE_URL,
+  databaseUrl: process.env.CLEARDB_DATABASE_URL || process.env.DATABASE_URL,
 
   port: Number(process.env.PORT || 8080),
   host: process.env.HOST || 'localhost',
