e642cc33581a78562354d20c3f4e8f6ae36fcc1a
Jaakko Husso <jaakko.husso@futurice.com>
2017-05-22 17:28:56 +0300

    Add database connection

--- src/services/search.js
+++ src/services/search.js
@@ -1,4 +1,10 @@
-const searchByName = name => Promise.resolve(name);
+// Initialize knex connection.
+const Knex = require('knex');
+const knexConfig = require('../config/knexfile');
+
+const knex = Knex(knexConfig[process.env.NODE_ENV || 'development']);
+
+const searchByName = name => knex.select().from('creature_template').where('name', name);
 
 module.exports = {
   searchByName,
